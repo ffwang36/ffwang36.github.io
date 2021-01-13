db50849f3d4bbeb04608be5899588c017d331a40
ffwang36 <61828681+ffwang36@users.noreply.github.com>
2021-01-13 21:45:29 +1000

    Create nameandColor.js

---
+++ nameandColor.js
@@ -0,0 +1,6 @@
+function nameAndColor() {
+	var username = prompt("what is your name? ");
+	var usercolor = prompt ('what is your fav color?');
+	document.write ("hello " + username +". <br>");
+	document.write ("you have won a " + usercolor + 'phone case');
+}
